6ee89952cf14ed16e65a11a7447fbce9ae7f0db3
Aon-Sao <larsatracy@gmail.com>
2024-07-15 21:41:46 -0500

    don't user extract univers

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import scrape_ascent
 import extract_neighborhood_sales_from_ascent_data
 import scrape_univers
-import extract_data_from_univers_html
 import extract_data_from_ascent_and_univers_html
 
 
@@ -13,7 +12,6 @@ def main():
     scrape_ascent.main()
     extract_neighborhood_sales_from_ascent_data.main()
     scrape_univers.main()
-    extract_data_from_univers_html.main()
     extract_data_from_ascent_and_univers_html.main()
 
 
