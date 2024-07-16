87e09869e489b93604f00e3e14e7e69688fccae6
Aon-Sao <larsatracy@gmail.com>
2024-07-15 21:42:19 -0500

    print less

--- scrape_univers.py
+++ scrape_univers.py
@@ -19,10 +19,10 @@ def main():
             # https://datagy.io/python-remove-first-n-characters-from-string/
             LongParcelNumber = row['ParcelNumber']
             ShortParcelNumber = LongParcelNumber[4:]
-            print(ShortParcelNumber)
+            # print(ShortParcelNumber)
             query = requests.get(
                 "http://www.caledonia.univers-clt.com/view_property_R.php?account_no=" + ShortParcelNumber + "&series_card=1")
-            print(query)
+            # print(query)
             # https://www.easytweaks.com/python-write-to-text-file/
             # write file names with long parcel number to ease compatibility when merging data from Ascent and Univers files
             with open("./data/" + LongParcelNumber + "_" + row["SiteAddress"] + ".html", mode="w") as f:
